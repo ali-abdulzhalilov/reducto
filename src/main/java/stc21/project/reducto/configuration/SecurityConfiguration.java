3c44286b373b5315f82a32af22aebd4eb4d5b4d8
mark-solo <mark.an.impostor@gmail.com>
2019-12-05 01:46:47 +0300

    Configured custom login page (God bless csrf)

--- src/main/java/stc21/project/reducto/configuration/SecurityConfiguration.java
+++ src/main/java/stc21/project/reducto/configuration/SecurityConfiguration.java
@@ -28,12 +28,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
+                    .loginPage("/login")
                     .permitAll()
+                    .defaultSuccessUrl("/", true)
+                    .failureForwardUrl("/register")
                     .and()
                 .logout()
                     .permitAll();
